a2aa00305626bcf41052c4733c86bd03a5cf8c21
Valeriy Van <github@w7software.com>
2018-03-07 23:48:00 +0100

    Removes boilerplate code

--- Sample Code/SwiftSampleCode/UILib Sample/WidgetsViewController.swift
+++ Sample Code/SwiftSampleCode/UILib Sample/WidgetsViewController.swift
@@ -12,28 +12,7 @@ import DJIUILibrary
 class WidgetsViewController: UIViewController {
     
     var widgets = [AnyObject]()
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    
     @IBAction func close () {
         self.dismiss(animated: true) {
             
